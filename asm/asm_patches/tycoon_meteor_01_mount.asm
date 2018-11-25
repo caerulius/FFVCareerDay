42c847cc8cb7a1c7af11b3252dcd7676500c3955
unknown <cleartonic@gmail.com>
2018-11-25 17:03:43 -0500

    update

--- asm/asm_patches/tycoon_meteor_01_mount.asm
+++ asm/asm_patches/tycoon_meteor_01_mount.asm
@@ -3,19 +3,13 @@ hirom
 
 ;  Boco dismount Tycoon 
 org $C84FD2
-; org $C851E2
 
-db $87, $09 ; show sprite for choco. $8x refers to active sprites
-db $87, $20 ; choco pose
-db $0a
-db $b1
-db $02
-db $10
-db $09
-db $05, $01 ; player bounce up
-db $a5,$ff ; clear event flag. as of now, not messing with these. if it was in the original function, keep 
-db $ff
+db $87, $09	; show sprite for choco. $8x refers to active sprites
+db $87, $20	; choco pose
+db $b1, $02	; set player sprite to butz
+db $05, $01	; player bounce up
+db $a5, $ff	; clear (some) event flag. as of now, not messing with these. if it was in the original function, keep 
+db $ff		; end event (this event). this is a terminator. 
 
-fillbyte $00
-fill 15
-; 0000000000000000000000000000
+fillbyte $00 ; this is for cleanliness only, the rest of this code wouldn't be called
+fill 18
