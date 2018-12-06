b5479b6a4407eeb3de84d0578670787afed8cbe5
Caerulius <cae@polywhack.com>
2018-12-05 22:31:26 -0700

    after karnak escape

--- asm/asm_patches/after_karnak_escape.asm
+++ asm/asm_patches/after_karnak_escape.asm
@@ -4,7 +4,10 @@ hirom
 ; Cutscene and job acquisition after Karnak
 org $C87F79
 
+db $7C							;<Unknown>
 db $E1, $A3, $00, $21, $08, $00	;Return from cutscene? A3 00 21 08 00
+db $BE, $00						;Rumble effect of 00 magnitude
+db $D9, $0A, $03, $70			;Unknown (im guessing this stops the timer)
 db $B1, $02						;Set Player Sprite 02
 db $09							;Player Show
 db $23							;Sprite 080 do event: face down, right hand raised out
@@ -30,7 +33,10 @@ db $08							;<Unknown>
 db $C8, $75, $02				;Display Message/Text/Dialogue 75 02
 db $87, $04						;Sprite 087 do event: Move Left
 db $87, $0A						;Sprite 087 do event: Hide
-db $73
+db $73							;Medium pause
+db $C6, $08						;Add job Mystic Knight
+db $C6, $03						;Add job Dragoon
+db $C6, $16						;Add job 16
 db $C6, $08						;Add job Mystic Knight
 db $C6, $03						;Add job Dragoon
 db $C6, $16						;Add job 16
@@ -38,8 +44,6 @@ db $A4, $E4						;Set Event Flag 1E4
 db $A4, $19						;Set Event Flag 119
 db $CB, $81, $01				;Clear Flag 2/3/4/5/81 01
 db $CA, $9D, $02				;Set Flag 2/3/4/5/9D 02
-db $B4, $29						;Play Background Music Fanfare 1 (short)
-db $C5, $E0, $E4				;<unknown>
 db $E3, $00, $00, $53, $4E, $00	;Inter-map cutscene? 00 00 53 4E 00
 db $DB							;Restore Player status
 db $B4, $23						;Play Background Music Four Valiant Hearts
