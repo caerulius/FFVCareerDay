f4e6caa693dd65c945f41d31ba75fc89c3b228ab
unknown <cleartonic@gmail.com>
2018-12-27 20:02:12 -0500

    add shiva, ifrit rewards

--- asm/asm_patches/world1/ifrit.asm
+++ asm/asm_patches/world1/ifrit.asm
@@ -10,8 +10,13 @@ db $A4, $28				;Set Event Flag 128
 db $CB, $97, $01		;Clear Flag 2/3/4/5/97 01
 db $83, $03				;Sprite 083 do event: Move Down
 db $83, $0A				;Sprite 083 do event: Hide
-db $C8, $A7, $02		;Display Message/Text/Dialogue A7 02
-db $AC, $4D				;Add Magic Ifrit
+if !vanillarewards == 1
+	db $C8, $A7, $02		;Display Message/Text/Dialogue A7 02
+	db $AC, $4D				;Add Magic Ifrit
+else
+	db $DE, $36				; set up reward
+	db $DF					; call text handler
+endif
 db $FF					;End Event
 
 padbyte $00
