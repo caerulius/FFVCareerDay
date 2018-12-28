eb27f472937064a6052afe1c7c9f22ab7fe0bf14
unknown <cleartonic@gmail.com>
2018-12-27 19:37:08 -0500

    fix blue learning, add pitfalls always on

--- asm/asm_patches/misc/utility.asm
+++ asm/asm_patches/misc/utility.asm
@@ -7,10 +7,15 @@ hirom
 
 
 ; Learning/Passages
-; Original values: $A9, $10, $EA
-org $C24C87
-db $B9, $20, $20
 
+; Learning in battles will always pass the check against original 'AND #$10' check
+org $C24C87
+lda #$10
+nop
 ; Passages always on
 org $C05AB8
 jmp $5ada
+; Pitfalls always on 
+org $C015B5
+lda #$02
+nop
