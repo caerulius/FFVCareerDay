734554f5493d7beb30e3b318aef23a4ef2f98957
Caerulius <cae@polywhack.com>
2019-01-10 14:34:58 -0700

    better printing solutions

--- data/collectible.py
+++ data/collectible.py
@@ -1,9 +1,12 @@
+from enum import Enum
+
 class Collectible:
     def __init__(self, id, name, location, collectible_type, \
                  type_address, type_data, item_address, item_data, kind):
         self.id = id
         self.name = name
-        self.original = name
+        self.original_name = name
+        self.original_collectible_type = collectible_type
         self.location = location
         self.collectible_type = collectible_type
         self.type_address = type_address
@@ -13,8 +16,9 @@ class Collectible:
         self.kind = kind
 
     def __str__(self):
-        return self.name + " " + self.collectible_type + " from " + self.location + \
-               " (" + self.item_address + ": " + self.item_data + ")"
+        return self.original_name + " " + self.original_collectible_type + \
+               " at " + self.location + " -> " + self.name + " " + \
+               self.collectible_type + "\n"
 
 
     def patch_code(self):
@@ -23,6 +27,18 @@ class Collectible:
             code += "org $" + self.type_address
             code += "\ndb $" + self.type_data + "\n"
         code += "org $" + self.item_address
-        code += "\ndb $" + self.item_data
+        code += "\ndb $" + self.item_data + "\n\n"
 
         return code
+
+class C_Type(Enum):
+    ITEM = 1,
+    GIL = 2,
+    CRYSTAL = 3,
+    MAGIC = 4,
+    MONSTER = 5
+
+class C_Kind(Enum):
+    REWARD = 1,
+    LOOT = 2,
+    SHOP = 3
--- data/rando_conductor.py
+++ data/rando_conductor.py
@@ -9,13 +9,12 @@ class RandoConductor():
         self.collectibles = []
         for index, row in self.collectibles_file.iterrows():
             self.collectibles.append(Collectible(index-1, row.readable_name, row.readable_location, \
-                                    row.type, row.type_address, row.type_data, \
-                                    row.item_address, row.item_data, row.kind))
+                                    row.type.upper(), row.type_address, row.type_data, \
+                                    row.item_address, row.item_data, row.kind.upper()))
 
     def shuffle_all_within_kind(self):
-        self.shuffle_kind("Reward")
-        self.shuffle_kind("Shop")
-        self.shuffle_kind("Loot")
+        for k in C_Kind:
+            self.shuffle_kind(k.name)
 
     def shuffle_kind(self, kind, iterations=5):
         kl = [x for x in self.collectibles if x.kind == kind]
@@ -37,11 +36,8 @@ class RandoConductor():
                     self.collectibles[result[1].id] = result[1]
 
     def shuffle_all_within_type(self):
-        self.shuffle_type("Gil")
-        self.shuffle_type("Item")
-        self.shuffle_type("Monster")
-        self.shuffle_type("Magic")
-        self.shuffle_type("Crystal")
+        for t in C_Type:
+            self.shuffle_type(t.name)
 
     def shuffle_type(self, c_type, iterations=5):
         cl = [x for x in self.collectibles if x.collectible_type == c_type]
@@ -66,13 +62,32 @@ class RandoConductor():
         c2.kind = temp.kind
 
         return (c1, c2)
-        
 
+    def get_patch(self):
+        patch = ""
+        for i in conductor.collectibles:
+            patch += i.patch_code()
+
+        return patch
+
+    def get_spoiler_log(self):
+        log = ""
+        for i in conductor.collectibles:
+            log += str(i)
+            
+        return log
+
+    def get_output(self):
+        patch = ""
+        log = ""
+        for i in conductor.collectibles:
+            patch += i.patch_code()
+            log += str(i)
+
+        return (patch, log)
 
 conductor = RandoConductor('collectibles.csv')
 conductor.shuffle_all_within_type()
 
-for i in conductor.collectibles:
-    print(i, end="")
-    print(" (started as: " + i.original + ")")
-    print(i.patch_code())
+#print(conductor.get_patch())
+print(conductor.get_spoiler_log())
